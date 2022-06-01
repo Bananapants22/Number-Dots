29907c134ff908d61f454dbb2c49534950bbb674
Harry Laptop <harry@harrycollins.co.uk>
2022-06-01 12:14:48 +0100

    Updated to code to make it faster, and moved the generating function to its own module. Also tidied up the old code and sample images to their own folders.

---
+++ number_generators.py
@@ -0,0 +1,18 @@
+"""A collection of functions to generate lists of numbers, usually up to a maximum value n."""
+import math
+
+def gen_primes(n: int) -> list:
+    """Generate all the prime numbers not greater than n using the Sieve of Eratosthenes."""
+    prime = [True for p in range(n+1)]
+
+    for i in range(2, int(math.sqrt(n)+1)):
+        if prime[i] == True:
+            for j in range(i**2, n+1, i):
+                prime[j] = False
+    
+    primes = []
+    for k in range(2, len(prime)):
+        if prime[k] == True:
+            primes.append(k)
+
+    return primes
---
+++ old_code/main_v1.py
@@ -0,0 +1,29 @@
+from tkinter.tix import Tree
+from PIL import Image
+
+width = 1920
+height = 1080
+
+def gen_primes(p):
+    for i in range(2, width*height):
+        prime = True
+        for entry in p:
+            if i % entry == 0:
+                prime = False
+        if prime:
+            p.append(i)
+
+primes = []
+gen_primes(primes)
+print("Prime List Generated")
+
+img = Image.new('1', (width, height))
+for x in range(1, width+1):
+    for y in range(height):
+        current_num = x + 10*y
+        if current_num in primes:
+            img.putpixel((x,y),1)
+filename = 'prime_square_' + str(width) + 'x' + str(height) + '.png'
+img.save(filename)
+
+print("Image File Generated")
